2d195095a53574d1165bd5012565c8b02f579639
mahamoutdogoy <32535223+mahamoutdogoy@users.noreply.github.com>
2018-10-01 14:43:07 +0530

    login password

--- mylogin.py
+++ mylogin.py
@@ -1,6 +1,6 @@
 import requests
 
 target_url = "https://courses.christuniversity.in/login/index.php"
-data_dict = {"username":"1641034" , "password": "81413582" , "loginbtn": "submit"}
+data_dict = {"username":"1641034" , "password": "" , "loginbtn": "submit"}
 response = requests.post(target_url, data=data_dict)
 print(response.content)
